1aaf6ef83580a415b8e8b149aa378bc079b90228
Renata Margula <reniutaaa@gmail.com>
2021-05-11 22:07:08 +0200

    remove tasks

--- js/script.js
+++ js/script.js
@@ -11,6 +11,18 @@
         },
     ];
 
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        });
+        render();
+    };
+
+    const removeTask = (taskIndex) => {
+        tasks.splice(taskIndex, 1);
+        render();
+    }
+
     const render = () => {
         let htmlString = "";
 
@@ -19,20 +31,24 @@
         <li
         ${task.done ? " style=\"text-decoration: line-through\"" : ""}
         >
+        <button class="js-remove">usuń</button>
         ${task.content}
         </li>
         `;
         }
 
         document.querySelector(".js-tasks").innerHTML = htmlString;
+
+        const removeButtons = document.querySelectorAll(".js-remove");
+
+        removeButtons.forEach((removeButton, index) => {
+            removeButton.addEventListener("click", () => {
+                removeTask(index);
+            });
+
+        });
     };
 
-    const addNewTask = (newTaskContent) => {
-        tasks.push({
-            content: newTaskContent,
-        });
-        render();
-    };
     const onFormSubmit = (event) => {
         event.preventDefault();
 
@@ -41,7 +57,6 @@
         if (newTaskContent === "") {
             return;
         }
-
 
         addNewTask(newTaskContent);
 
