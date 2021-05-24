4fcd9109bac5462b568fcdeb727f027bf8b51fb9
Renata Margula <reniutaaa@gmail.com>
2021-05-24 21:23:59 +0200

    refactoring js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
 {
-    const tasks = [];
+    let tasks = [];
+    let hideDoneTasks = false;
 
     const addNewTask = (newTaskContent) => {
-        tasks.push({
-            content: newTaskContent,
-        });
+        tasks = [
+            ...tasks,
+            { content: newTaskContent, done: false }
+        ];
+
         render();
     };
     const clearNewTaskInput = (newTaskInput) => {
@@ -12,14 +15,37 @@
         newTaskInput.value = "";
     };
     const removeTask = (taskIndex) => {
-        tasks.splice(taskIndex, 1);
+        tasks = [
+            ...tasks.slice(0, taskIndex),
+            ...tasks.slice(taskIndex + 1),
+        ];
         render();
-    }
-
+    };
     const toggleTaskDone = (taskIndex) => {
-        tasks[taskIndex].done = !tasks[taskIndex].done;
+        const task = tasks[taskIndex];
+        tasks = [
+            ...tasks.slice(0, taskIndex),
+            {
+                ...task,
+                done: !task.done
+            },
+            ...tasks.slice(taskIndex + 1),
+        ];
         render();
-    }
+    };
+    const markAllTasksDone = () => {
+        tasks = tasks.map(task => ({
+            ...task,
+            done: true,
+        }));
+        render();
+    };
+    const disableAllDoneButton = (button) => {
+        const allTasksDone = tasks.every(({ done }) => done === true);
+        if (allTasksDone) {
+            button.disabled = true;
+        }
+    };
     const bindEvents = () => {
         const removeButtons = document.querySelectorAll(".js-remove");
 
@@ -35,32 +61,57 @@
                 toggleTaskDone(index);
             });
         });
-    }
 
-    const render = () => {
+        const hideDoneTasksButton = document.querySelector(".js-allDoneHidden");
+        hideDoneTasksButton.addEventListener("click", () => {
+            hideDoneTasks = !hideDoneTasks;
+            render();
+        });
+
+        const markAllDoneTasksButton = document.querySelector(".js-markAllDone");
+        disableAllDoneButton(markAllDoneTasksButton)
+        markAllDoneTasksButton.addEventListener("click", () => {
+            markAllTasksDone();
+        });
+
+    }
+    const renderTasks = () => {
         let htmlString = "";
 
         for (const task of tasks) {
             htmlString += `
-        <li 
-            class="list__tasks"
-          >
-            <button class="list__button list__button--done js-done">
-              ${task.done ? "✓" : ""}</button>
-            
-             <span class="list__item${task.done ? " list__item--done" : ""}">
-              ${task.content}
-             </span>
-             <button class="list__button list__button--remove js-remove">🗑</button>
-        </li>
-        `;
-        }
+<li
+    class="list__tasks${task.done & hideDoneTasks ? " list__item--hidden" : ""}"> 
+    <button class="list__button list__button--done js-done">
+      ${task.done ? "✓" : ""}
+      </button>
+     <span class="list__item${task.done ? " list__item--done" : ""}">
+      ${task.content}
+     </span>
+     <button class="list__button list__button--remove js-remove">🗑</button>
+</li>
+`;
+        };
 
         document.querySelector(".js-tasks").innerHTML = htmlString;
-
+    };
+    const renderButtons = () => {
+        let htmlButtonsString = "";
+        htmlButtonsString +=
+            `<button class="section__button section__button--header ${tasks.length === 0 ? "section__button--hidden" : ""} js-allDoneHidden">
+            ${hideDoneTasks ? "Pokaż ukończone" : "Ukryj ukończone"}
+        </button>
+        <button class="section__button section__button--header  ${tasks.length === 0 ? "section__button--hidden" : ""} js-markAllDone">
+            Ukończ wszystkie
+        </button>
+    `;
+        document.querySelector(".js-buttons").innerHTML = htmlButtonsString;
+    };
+    const render = () => {
+        renderTasks();
+        renderButtons();
         bindEvents();
     };
-
     const onFormSubmit = (event) => {
         event.preventDefault();
 
